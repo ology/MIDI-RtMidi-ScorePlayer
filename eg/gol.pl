9f332a246d7ece29408baac28916ffae5f9ed894
Gene Boggs <gene@ology.net>
2024-07-13 01:19:37 -0700

    This is hideous noise

---
+++ eg/gol.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Game::Life::Faster ();
+use MIDI::RtMidi::ScorePlayer ();
+use MIDI::Util qw(setup_score set_chan_patch);
+use Music::Scales qw(get_scale_MIDI);
+use Term::ANSIScreen qw(cls);
+
+my $size = shift || 30;
+
+my $game = Game::Life::Faster->new($size);
+
+my $matrix = [ map { [ map { int(rand 2) } 1 .. $size ] } 1 .. $size ];
+$game->place_points(0, 0, $matrix);
+
+my @parts = (\&part) x $size;
+
+while (1) {
+    cls();
+    my @grid = $game->get_text_grid;
+    my $grid = $game->get_text_grid;
+
+    my $score = setup_score(lead_in => 0);
+    my %common = (score => $score, grid => \@grid, size => $size, seen => {});
+
+    print scalar $grid, "\n";
+
+    MIDI::RtMidi::ScorePlayer->new(
+      score    => $score,
+      parts    => \@parts,
+      common   => \%common,
+      sleep    => 0,
+      infinite => 0,
+    )->play;
+
+    $game->process;
+    last unless $game->get_used_text_grid;
+}
+
+sub part {
+    my (%args) = @_;
+
+    my $patch = int rand 20;
+    my $track = $args{size} - $args{_part};
+    my $channel = $args{_part} < 9 ? $args{_part} : $args{_part} + 1;
+    my $octave = ($args{_part} % 5) + 1;
+    my @scale = (
+        get_scale_MIDI('C', $octave, 'chromatic'),
+    );
+    my @row = split //, $args{grid}->[$track];
+
+    my $part = sub {
+        set_chan_patch($args{score}, $channel, $patch);
+
+        my @pitches;
+        for my $i (0 .. $args{size} - 1) {
+            if ($row[$i] eq 'X') {
+                my $pitch = $scale[$i];
+                push @pitches, $pitch
+                    unless $args{seen}->{$pitch}++;
+            }
+        }
+        if (@pitches) {
+            $args{score}->n('qn', @pitches);
+        }
+    };
+
+    return $part;
+}
