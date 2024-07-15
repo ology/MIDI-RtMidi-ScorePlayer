3efee71a772961df91f4ed6aa0009240ba891cf2
Gene Boggs <gene@ology.net>
2024-07-14 23:02:19 -0700

    Examples

--- eg/gol.pl
+++ eg/gol.pl
@@ -12,6 +12,8 @@ use warnings;
 # perl eg/dat/gol.pl 12 eg/dat/gol-12-blink.dat
 # perl eg/dat/gol.pl 12 eg/dat/gol-5-4x.dat
 # perl eg/dat/gol.pl 12 1 # render glider
+# perl eg/dat/gol.pl 7 2  # render toad
+# perl eg/dat/gol.pl 7 3  # render beacon
 
 use Game::Life::Faster ();
 use MIDI::RtMidi::ScorePlayer ();
