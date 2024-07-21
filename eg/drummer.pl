f21d748669869c834138a7b8394ee89f378c3905
Gene Boggs <gene@ology.net>
2024-07-21 16:13:53 -0700

    State

--- eg/drummer.pl
+++ eg/drummer.pl
@@ -20,7 +20,8 @@ my @parts;
 my $bpm  = 100;
 my $dura = 'qn';
 my $mode = 'serial';
-print "State: BPM=$bpm, Duration=$dura, Mode=$mode\n";
+my $repeats = 1;
+print "State: BPM=$bpm, Duration=$dura, Mode=$mode, Rpeats=$repeats\n";
 my $loop = IO::Async::Loop->new;
 my $tka  = Term::TermKey::Async->new(
   term   => \*STDIN,
