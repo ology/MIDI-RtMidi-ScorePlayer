25b94a31d6704ebc353f08861d0e66c80072fdf0
Gene Boggs <gene@ology.net>
2024-07-21 16:31:02 -0700

    Shrink
    ~

--- eg/drummer.pl
+++ eg/drummer.pl
@@ -17,10 +17,7 @@ my $verbose = shift || 0;
 
 my %common;
 my @parts;
-my $bpm  = 100;
-my $dura = 'qn';
-my $mode = 'serial';
-my $repeats = 1;
+my ($bpm, $dura, $mode, $repeats) = (100, 'qn', 'serial', 1);
 print "State: BPM=$bpm, Duration=$dura, Mode=$mode, Repeats=$repeats\n";
 my $loop = IO::Async::Loop->new;
 my $tka  = Term::TermKey::Async->new(
